4f49308d7c1665eff16c0229d0cea682f3fa8198
Alexander Buiko <reed665@hotmail.com>
2018-05-04 15:52:35 +0300

    GMap: setup google map

--- src/views/GMap.vue
+++ src/views/GMap.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="gmap container">
-    <h2>GMap</h2>
+    <div class="google-map" id="map"></div>
   </div>
 </template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      lat: 53,
+      lng: -2,
+    }
+  },
+  mounted () {
+    this.renderMap()
+  },
+  methods: {
+    renderMap () {
+      const mapEl = document.getElementById('map')
+      const map = new google.maps.Map(mapEl, {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false,
+      })
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .google-map {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+</style>
