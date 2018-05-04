3e2b4e4c70aad5b1c9b61f3b3cfa5946bf2fcb31
Alexander Buiko <reed665@hotmail.com>
2018-05-04 17:33:58 +0300

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,13 @@
 import firebase from 'firebase'
-import firestore from 'firebase/firestore'
+import firestore from 'firebase/firestore' // eslint-disable-line no-unused-vars
 
 const config = {
-  apiKey: "AIzaSyBBti1eBtVN1XHLDIr4Boa_TXFYUH6Fneg",
-  authDomain: "vue-geo-ninjas-bc962.firebaseapp.com",
-  databaseURL: "https://vue-geo-ninjas-bc962.firebaseio.com",
-  projectId: "vue-geo-ninjas-bc962",
-  storageBucket: "vue-geo-ninjas-bc962.appspot.com",
-  messagingSenderId: "678983538094"
-};
-firebase.initializeApp(config);
+  apiKey: 'AIzaSyBBti1eBtVN1XHLDIr4Boa_TXFYUH6Fneg',
+  authDomain: 'vue-geo-ninjas-bc962.firebaseapp.com',
+  databaseURL: 'https://vue-geo-ninjas-bc962.firebaseio.com',
+  projectId: 'vue-geo-ninjas-bc962',
+  storageBucket: 'vue-geo-ninjas-bc962.appspot.com',
+  messagingSenderId: '678983538094',
+}
 
-const firebaseApp = firebase.initializeApp(config)
-
-export default firebaseApp.firestore()
+export default firebase.initializeApp(config).firestore()
